4193d91032764d4846d4f4ddcb8285b7b1894d56
nashidfarhad <nashidfarhad@users.noreply.github.com>
2017-08-22 15:50:33 +1000

    WIP

--- tdtask.js
+++ tdtask.js
@@ -2,13 +2,14 @@ class tdtask {
 	var task : String;
 	var createdDate : Date;
 	var endDate : Date;
-	var dueDate : Date;
-	var taskID : String;
-	var PID : String;
+	var dueDate : Date; // keyvalue [due:YYYY-MM-DD]
+	var taskID : String; // system generated ID to track each task, keyvalue [id:String]
+	var PID : String; // to store the parent task ID, keyvalue [pid:String]
 	var project : Array;
 	var context : Array;
 	var priority : String;
 	var isDone : boolean;
+	var notes : String; // to store additional notes on a task, keyvalue [notes:String]
 		
 	constructor(task, createdDate, endDate, dueDate, taskID, PID, project, context, priority, isDone) {
 		this.task = task;
--- tdtasks.js
+++ tdtasks.js
@@ -4,6 +4,8 @@ class tdtasks {
 	constructor() {
 		this.tasks = new tdtask[];
 	}
+	
+	//function to push a task into the array, takes a tdtask object
 	function push(task){
 		this.tasks.push(task);
 	}
