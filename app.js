f29708d985e3f8b7341fd60992a33a69f8aad14f
Navid Forhad <nforhad@gmail.com>
2017-08-26 19:25:31 +0600

    converted app.js code to react code

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const path = require('path')
 const fs = require('fs');
 
-document.addEventListener("DOMContentLoaded", function () {
-    const btnLoadFile = document.getElementById('btn-load-file');
-
-    btnLoadFile.addEventListener('click', function (event) {
-        console.log(__dirname);
-
+class App extends React.Component{
+  loadFile(){
         fs.readFile(path.join(__dirname, 'TODO.txt'), function (err, data) {
             if (err) {
                 return console.error(err);
             }
             document.getElementById('file-content').innerText = data.toString();
         });
-    });
-});
+  }
+  render(){
+    return(
+      <div>
+      <h1>Hello World</h1>
+      <button onClick={this.loadFile}>Load File</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+)
