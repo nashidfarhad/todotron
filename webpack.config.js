3fad056a70f8b85c8b18e4f14698d07350cffa9b
Navid Forhad <nforhad@gmail.com>
2017-08-27 19:40:51 +0600

    modified webpack config to handle jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 module.exports = {
-  entry: __dirname + '/app.js',
-  node:{
-    __filename:true,
-    __dirname:true
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
-  },
-  output:{
-    filename:'app.bundle.js',
-    path:__dirname+'/dist'
-  },
-  target:'node'
+    entry: __dirname + '/app/app.js',
+    node: {
+        __filename: true,
+        __dirname: true
+    },
+    module: {
+        loaders: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    output: {
+        filename: 'app.bundle.js',
+        path: __dirname + '/dist'
+    },
+    target: 'node',
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".js", ".json", ".jsx"]
+    }
 }
