5c8239c25b7c53f21f2a2b52cd72b60b558f9b1d
nashidfarhad <nashidfarhad@users.noreply.github.com>
2017-08-27 18:12:07 +1000

    after merge

--- app.js
+++ app.js
@@ -4,26 +4,8 @@ import ReactDOM from 'react-dom';
 const path = require('path')
 const fs = require('fs');
 
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-class MyComponentClass extends React.Component{
-    render(){
-        return <h1> Test </h1>;
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    const btnLoadFile = document.getElementById('btn-load-file');
-
-    btnLoadFile.addEventListener('click', function (event) {
-        console.log(__dirname);
-
-=======
 class App extends React.Component{
   loadFile(){
->>>>>>> 1c331c0fa86729a2da47225d562830dcb7f4b888
         fs.readFile(path.join(__dirname, 'TODO.txt'), function (err, data) {
             if (err) {
                 return console.error(err);
@@ -31,12 +13,6 @@ class App extends React.Component{
             document.getElementById('file-content').innerText = data.toString();
             
         });
-<<<<<<< HEAD
-    });
-});
-
-ReactDOM.render(<MyComponentClass />,document.getElementById('test-div'));
-=======
   }
   render(){
     return(
@@ -52,4 +28,3 @@ ReactDOM.render(
   <App/>,
   document.getElementById('app')
 )
->>>>>>> 1c331c0fa86729a2da47225d562830dcb7f4b888
