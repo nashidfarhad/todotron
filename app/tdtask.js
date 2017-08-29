8caaa59af47583a6798afb6f2efbaa5c5c95610a
nashidfarhad <nashidfarhad@users.noreply.github.com>
2017-08-29 20:19:09 +1000

    no need to define variables in TdTask

--- app/tdtask.js
+++ app/tdtask.js
@@ -1,17 +1,17 @@
 class TdTask {
-	var task : String;
-	var createdDate : Date;
-	var endDate : Date;
-	var dueDate : Date; // keyvalue [due:YYYY-MM-DD]
-	var taskID : String; // system generated ID to track each task, keyvalue [id:String]
-	var PID : String; // to store the parent task ID, keyvalue [pid:String]
-	var project : Array;
-	var context : Array;
-	var priority : String;
-	var isDone : boolean;
-	var notes : String; // to store additional notes on a task, keyvalue [notes:String]
-	var recur : String;
-	var repeat : int;
+	// var task : String;
+	// var createdDate : Date;
+	// var endDate : Date;
+	// var dueDate : Date; // keyvalue [due:YYYY-MM-DD]
+	// var taskID : String; // system generated ID to track each task, keyvalue [id:String]
+	// var PID : String; // to store the parent task ID, keyvalue [pid:String]
+	// var project : Array;
+	// var context : Array;
+	// var priority : String;
+	// var isDone : boolean;
+	// var notes : String; // to store additional notes on a task, keyvalue [notes:String]
+	// var recur : String;
+	// var repeat : int;
 
 	constructor(task, createdDate, endDate, dueDate, taskID, PID, project, context, priority, isDone) {
 		this.task = task;
