a994059a6d27fae3a1ed8be5a0e64a6acb4152e9
Navid Forhad <nforhad@gmail.com>
2017-08-29 10:11:05 +0600

    added toString method for converting task to todo.txt format string

--- app/tdtask.js
+++ app/tdtask.js
@@ -12,7 +12,7 @@ class TdTask {
 	var notes : String; // to store additional notes on a task, keyvalue [notes:String]
 	var recur : String;
 	var repeat : int;
-		
+
 	constructor(task, createdDate, endDate, dueDate, taskID, PID, project, context, priority, isDone) {
 		this.task = task;
 		this.createdDate = createdDate;
@@ -25,5 +25,37 @@ class TdTask {
 		this.priority = priority;
 		this.isDone = isDone;
 	}
-	
+
+	/*
+	 * this method will allow string generation of task to
+	 * write in todo.txt file
+	 */
+	toString() {
+		let taskString = '';
+		if(isDone)
+			taskString += 'x ';
+		if(priority != null && priority.length !== 0)
+			taskString += `(${priority}) `;
+		if(endDate != null && endDate.length !== 0)
+			taskString += `${endDate} `;
+
+		taskString += `${createdDate} ${task} `;
+
+		if(taskID != null && taskID.length !== 0)
+			taskString += `id:${taskID} `;
+		if(PID != null && PID.length !== 0)
+			taskString = `pid:${PID} `;
+		if(notes != null && notes.length !== 0)
+			taskString= `notes:${notes} `;
+		if(dueDate != null && dueDate.length !== 0)
+			taskString += `due:${dueDate} `;
+		if(dueDate != null && dueDate.length !== 0)
+			taskString += `due:${endDate} `;
+		if(recur != null && recur.length !== 0)
+			taskString += `recur:${recur} `;
+		if(repeat != null)
+			taskString += `repeat:${repeat} `;
+		//TODO: append projects and contexts
+		return taskString;
+	}
 }
