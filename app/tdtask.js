935a6965c72bbd5e25e25e6768380d323cb45151
Navid Forhad <nforhad@gmail.com>
2017-08-29 17:10:45 +0600

    renamed project & context variables to projects & contexts

--- app/tdtask.js
+++ app/tdtask.js
@@ -13,15 +13,15 @@ class TdTask {
 	// var recur : String;
 	// var repeat : int;
 
-	constructor(task, createdDate, endDate, dueDate, taskID, PID, project, context, priority, isDone) {
+	constructor(task, createdDate, endDate, dueDate, taskID, PID, projects, contexts, priority, isDone) {
 		this.task = task;
 		this.createdDate = createdDate;
 		this.endDate = endDate;
 		this.dueDate = dueDate;
 		this.taskID = taskID;
 		this.PID = PID;
-		this.project = project;
-		this.context = context;
+		this.projects = projects;
+		this.contexts = contexts;
 		this.priority = priority;
 		this.isDone = isDone;
 	}
