e252f0ccd38e95fe4d72d5bf4d8416870eb940bd
Navid Forhad <nforhad@gmail.com>
2017-09-03 20:46:38 +0600

    added css class .icon to control icon related styling

--- app/components/icons/IconPlus.jsx
+++ app/components/icons/IconPlus.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 export class IconPlus extends React.Component {
 	render () {
-		const {color} = this.props;
+		const {color,classes} = this.props;
 		return (
-			<svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8">
+			<svg className={classes} xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8">
 				<path fill={color} d="M3 0v3h-3v2h3v3h2v-3h3v-2h-3v-3h-2z" />
 			</svg>
 		);
@@ -11,9 +11,11 @@ export class IconPlus extends React.Component {
 }
 
 IconPlus.defaultProps = {
-	color: 'black'
+	color: 'black',
+	classes: ''
 };
 
 IconPlus.propTypes = {
-	color: React.PropTypes.string
+	color: React.PropTypes.string,
+	classes: React.PropTypes.string
 };
