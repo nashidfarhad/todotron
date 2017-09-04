df0f612d6c8f3ec6ab86d618d5c01fe5456d03cb
Navid Forhad <nforhad@gmail.com>
2017-09-04 22:43:05 +0600

    added token and tokenType parameter in constructor

--- app/tasktoken.js
+++ app/tasktoken.js
@@ -1,8 +1,21 @@
 import { TokenTypes } from './tokentypes';
 
 export class TaskToken {
-	constructor() {
-		this.token = '';
-		this.tokenType = TokenTypes.NORMAL;
+	constructor(token, tokenType) {
+		if (token != null)
+			this.token = token;
+		else
+			this.token = '';
+
+		if (tokenType != null)
+			this.tokenType = tokenType;
+		else
+			this.tokenType = TokenTypes.NORMAL;
+
+		if (tokenType == TokenTypes.TAG) {
+			let tagTokens = token.split(':');
+			this.tagName = tagTokens[0];
+			this.tagValue = tagTokens[1];
+		}
 	}
 }
