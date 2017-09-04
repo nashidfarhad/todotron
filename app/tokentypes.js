a21b38e571eace12cf47aafdce5e7871473c8cbd
Navid Forhad <nforhad@gmail.com>
2017-09-04 22:41:38 +0600

    added priority token type

--- app/tokentypes.js
+++ app/tokentypes.js
@@ -1,6 +1,7 @@
 export const TokenTypes = Object.freeze({
-    NORMAL:   Symbol("normal"),
-    PROJECT:  Symbol("project"),
+	NORMAL:   Symbol("normal"),
+	PRIORITY:  Symbol("priority"),
+	PROJECT:  Symbol("project"),
 	COMPLETION: Symbol("completion"),
 	COMPLETION_DATE: Symbol("completion_date"),
 	CONTEXT: Symbol("context"),
