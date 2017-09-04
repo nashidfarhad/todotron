60e79d5dc2ec7fcf103aebb834e3f85ca9f0d426
Navid Forhad <nforhad@gmail.com>
2017-09-04 17:19:27 +0600

    added conditaional css class to rendered tdtask component

--- app/components/TdTaskComponent.jsx
+++ app/components/TdTaskComponent.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 export class TdTaskComponent extends React.Component {
 	render() {
+		let classNames = ['tdtask'];
+		if(this.props.tdtask.isDone) classNames.push('complete');
+		if(this.props.tdtask.isDue()) classNames.push('due');
 		return (
-			<div>{this.props.tdtask}</div>
+			<div className={classNames.join(' ')}>{this.props.tdtask.task}</div>
 		);
 	}
 }
