ca8e15a56f741f8f02087520718ed7904501b590
Navid Forhad <nforhad@gmail.com>
2017-09-06 20:23:09 +0600

    testing correct number of token parse

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -2,9 +2,15 @@ const electronRequire = require;
 import { Parser } from '../app/parser';
 import { TokenTypes } from '../app/tokentypes';
 
+const parser = new Parser('');
+
+test('parses correct number of tokens', () => {
+    let tdtaskline = "x 2016-09-06 bla bla bal jslfjsdlfjslfjl";
+    let tdtask = parser.parseTdTask(tdtaskline);
+    expect(tdtask.tokens.length).toBe(6);
+});
+
 test('x makes first tokens TokenType COMPLETION', () => {
-    global.electronRequire = function(){};
-    let parser = new Parser('');
     let tdtaskline = "x 2016-09-06 bla bla bal jslfjsdlfjslfjl";
     let tdtask = parser.parseTdTask(tdtaskline);
     expect(tdtask.tokens[0].tokenType).toBe(TokenTypes.COMPLETION);
