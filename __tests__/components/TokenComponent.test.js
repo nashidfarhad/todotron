3a932427574949261e1d1eb28d5f846afbda5ecd
Navid Forhad <nforhad@gmail.com>
2017-09-07 17:24:40 +0600

    wrote test for tokencomponent

---
+++ __tests__/components/TokenComponent.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { TokenComponent } from '../../app/components/TokenComponent';
+import { TaskToken } from '../../app/tasktoken';
+import { TokenTypes } from '../../app/tokentypes';
+
+describe('TokenComponent', () => {
+    test('shows token string correctly', () => {
+
+        let taskToken = new TaskToken('abc', TokenTypes.NORMAL);
+        // Render a checkbox with label in the document
+        const tokenComp = shallow( 
+            <TokenComponent token = { taskToken } />
+        );
+
+        expect(tokenComp.text()).toEqual('abc');
+    });
+    test('default css class is "token"', () => {
+
+        let taskToken = new TaskToken('abc', TokenTypes.NORMAL);
+        // Render a checkbox with label in the document
+        const tokenComp = shallow( 
+            <TokenComponent token = { taskToken } />
+        );
+
+        expect(tokenComp.find('span').hasClass('token')).toBe(true);
+    });
+    test('context token has css class "token" & "context"', () => {
+
+        let taskToken = new TaskToken('abc', TokenTypes.CONTEXT);
+        // Render a checkbox with label in the document
+        const tokenComp = shallow( 
+            <TokenComponent token = { taskToken } />
+        );
+
+        expect(tokenComp.find('span').hasClass('token')).toBe(true);
+        expect(tokenComp.find('span').hasClass('context')).toBe(true);
+    });
+    test('project token has css class "token" & "project"', () => {
+
+        let taskToken = new TaskToken('abc', TokenTypes.PROJECT);
+        // Render a checkbox with label in the document
+        const tokenComp = shallow( 
+            <TokenComponent token = { taskToken } />
+        );
+
+        expect(tokenComp.find('span').hasClass('token')).toBe(true);
+        expect(tokenComp.find('span').hasClass('project')).toBe(true);
+    });
+});
