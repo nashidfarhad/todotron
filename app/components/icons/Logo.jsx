0d235ea260e9eb46c90b7d926455d075a4b2eb21
Navid Forhad <nforhad@gmail.com>
2017-09-10 22:28:21 +0600

    added css classed for the logo

--- app/components/icons/Logo.jsx
+++ app/components/icons/Logo.jsx
@@ -4,12 +4,14 @@ import PropTypes from 'prop-types';
 export class Logo extends React.Component {
     render() {
         return (
-            <svg viewBox="-2 -2 130 138" className="icon" preserveAspectRatio="true">
-                <path d=" M 79.919 60 L 130 39.976 L 109.919 110 L 69.919 140 L 79.919 60 Z " fill={this.props.fillColor}/>
-                <path d=" M 10 39.976 L 59.919 60 L 69.919 140 L 29.919 110 L 10 39.976 Z " fill={this.props.fillColor}/>
-                <path d=" M 70 12 L 130 39.976 L 10 39.976 L 70 12 Z " fill={this.props.fillColor}/>
-                <path d=" M 10 40 L 130 40 L 80 60 L 70 140 L 60 60 L 10 40 Z " fill="rgb(74,134,232)"/>
-            </svg>
+            <div className="toolbar-logo">
+                <svg viewBox="-2 -2 130 138" className="logo" preserveAspectRatio="xMidYMid">
+                    <path d=" M 79.919 60 L 130 39.976 L 109.919 110 L 69.919 140 L 79.919 60 Z " fill={this.props.fillColor}/>
+                    <path d=" M 10 39.976 L 59.919 60 L 69.919 140 L 29.919 110 L 10 39.976 Z " fill={this.props.fillColor}/>
+                    <path d=" M 70 12 L 130 39.976 L 10 39.976 L 70 12 Z " fill={this.props.fillColor}/>
+                    <path d=" M 10 40 L 130 40 L 80 60 L 70 140 L 60 60 L 10 40 Z " fill="rgb(74,134,232)"/>
+                </svg>
+            </div>
         );
     }
 }
