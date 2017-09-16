b57e5a715ebb620c31b915e47f5ed07dd48d4549
Navid Forhad <nforhad@gmail.com>
2017-09-16 17:42:11 +0600

    removed unused fs import

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
-const fs = require('fs')
 require('electron-debug')({showDevTools: true});
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
